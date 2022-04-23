5a119a1a8ddbc47953e21e06672b2b38a957f519
manofearth <azallay@mail.ru>
2022-04-23 19:29:27 +0900

    first commit

---
+++ background.js
@@ -0,0 +1,14 @@
+const iconUrl = chrome.runtime.getURL("icon32.png");
+
+chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+  if (changeInfo.favIconUrl && tab.url.startsWith('https://app.slack.com/')) {
+    chrome.scripting.executeScript({
+      target: { tabId },
+      args: [iconUrl],
+      func: iconUrl => {
+        console.log('hello');
+        document.querySelector('link[rel~=\'icon\']').href = iconUrl;
+      }
+    })
+  }
+});
